5c40d1a78bac5753769e1c7a062ef81e8a2a15ec
Invernizzi-Fabian <fabian.invernizzi@edu.univ-fcomte.fr>
2024-04-12 16:16:45 +0200

    ajout v-show et pointerdown sur les sections

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+const sectionOpen = ref(0)
 </script>
 
 <template>
 <h1>Bonjour monde !</h1>
 
 <section>
-    <button class="text-xl">bouton 1</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reiciendis necessitatibus, quas culpa aut qui officia cum aspernatur? Nostrum fugit facilis atque sunt provident consequatur omnis at minima vero nemo?</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</button>
+    <p v-show="sectionOpen === 1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reiciendis necessitatibus, quas culpa aut qui officia cum aspernatur? Nostrum fugit facilis atque sunt provident consequatur omnis at minima vero nemo?</p>
 </section>
 <section>
-    <button class="text-xl">bouton 2</button>
-    <p>Maxime ullam commodi velit perferendis incidunt sapiente laudantium voluptates aperiam ad mollitia quo porro distinctio repudiandae soluta autem voluptas quod, possimus necessitatibus fugit quos. Soluta porro dicta voluptatibus eaque quod.</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</button>
+    <p v-show="sectionOpen === 2">Maxime ullam commodi velit perferendis incidunt sapiente laudantium voluptates aperiam ad mollitia quo porro distinctio repudiandae soluta autem voluptas quod, possimus necessitatibus fugit quos. Soluta porro dicta voluptatibus eaque quod.</p>
 </section>
 <section>
-    <button class="text-xl">bouton 3</button>
-    <p>Cumque aspernatur repellat ea fugiat earum, dolorem iusto distinctio cupiditate quasi enim dolorum modi sunt assumenda quis, necessitatibus nulla obcaecati tempore est quo. Debitis recusandae dignissimos aliquam doloremque id aperiam.</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</button>
+    <p v-show="sectionOpen === 3">Cumque aspernatur repellat ea fugiat earum, dolorem iusto distinctio cupiditate quasi enim dolorum modi sunt assumenda quis, necessitatibus nulla obcaecati tempore est quo. Debitis recusandae dignissimos aliquam doloremque id aperiam.</p>
 </section>
 
 </template>
